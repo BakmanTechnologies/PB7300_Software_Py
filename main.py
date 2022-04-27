2d66d055193ca8103b3d158ce95d1d327c445a9b
RFranco9 <rickfranco9@gmail.com>
2022-04-27 14:49:00 -0700

    dwell function tested and working right

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from serial_commands_PB7200 import SerialCommands
 from serial_data_manipulation import SerialDataManipulation
 from utils import read_json_from_file
-from cal_data import CalData
 
 
 def main():
     """Main function"""
     cal_data = read_json_from_file()
     serial_data_manipulation = SerialDataManipulation(cal_data)
-    serial_data_manipulation.dwell_control(800, 100)
+    serial_data_manipulation.dwell_control(400, 100)
     serial_data_manipulation.close_port()
 
 
--- serial_data_manipulation.py
+++ serial_data_manipulation.py
@@ -1,9 +1,15 @@
 from serial_commands_PB7300 import SerialCommands
 import hashlib
 import time
+from utils import simple_graph
 
 
 class SerialDataManipulation():
+    error_freq_last_ghz = 0
+    error_freq_i = 0
+    set_freq = 0
+    stable_count = 0
+
 
     def __init__(self, cal_data) -> None:
         self.cal_data = cal_data
@@ -68,98 +74,59 @@ class SerialDataManipulation():
         LASER_TEMP_MAX = 65
         LASER_TEMP_MIN = 5
 
-        laser_0_breakpoints_freq = []
-        laser_1_breakpoints_freq = []
+        laser_0_breakpoints_freq_1 = float(self.cal_data.LD0.upscan_breakpoint[1])
+        laser_0_breakpoints_freq_2 = float(self.cal_data.LD0.upscan_breakpoint[3])
+        laser_0_breakpoints_freq_3 = float(self.cal_data.LD0.upscan_breakpoint[5])
+        laser_1_breakpoints_freq_1 = float(self.cal_data.LD1.upscan_breakpoint[1])
+        laser_1_breakpoints_freq_2 = float(self.cal_data.LD1.upscan_breakpoint[3])
+        laser_1_breakpoints_freq_3 = float(self.cal_data.LD1.upscan_breakpoint[5])
 
-        laser_0_breakpoints_temp = []
-        laser_1_breakpoints_temp = []
+        laser_0_breakpoints_temp_1 = float(self.cal_data.LD0.upscan_breakpoint[0])
+        laser_0_breakpoints_temp_2 = float(self.cal_data.LD0.upscan_breakpoint[2])
+        laser_0_breakpoints_temp_3 = float(self.cal_data.LD0.upscan_breakpoint[4])
+        laser_1_breakpoints_temp_1 = float(self.cal_data.LD1.upscan_breakpoint[0])
+        laser_1_breakpoints_temp_2 = float(self.cal_data.LD1.upscan_breakpoint[2])
+        laser_1_breakpoints_temp_3 = float(self.cal_data.LD1.upscan_breakpoint[4])
 
-        laser_0_breakpoints_temp.append(self.cal_data.LD0.upscan_breakpoint[0])
-        laser_0_breakpoints_freq.append(self.cal_data.LD0.upscan_breakpoint[1])
-        laser_0_breakpoints_temp.append(self.cal_data.LD0.upscan_breakpoint[2])
-        laser_0_breakpoints_freq.append(self.cal_data.LD0.upscan_breakpoint[3])
-        laser_0_breakpoints_temp.append(self.cal_data.LD0.upscan_breakpoint[4])
-        laser_0_breakpoints_freq.append(self.cal_data.LD0.upscan_breakpoint[5])
+        #LD0
+        #Seg 1
 
-        laser_1_breakpoints_temp.append(self.cal_data.LD1.upscan_breakpoint[0])
-        laser_1_breakpoints_freq.append(self.cal_data.LD1.upscan_breakpoint[1])
-        laser_1_breakpoints_temp.append(self.cal_data.LD1.upscan_breakpoint[2])
-        laser_1_breakpoints_freq.append(self.cal_data.LD1.upscan_breakpoint[3])
-        laser_1_breakpoints_temp.append(self.cal_data.LD1.upscan_breakpoint[4])
-        laser_1_breakpoints_freq.append(self.cal_data.LD1.upscan_breakpoint[5])
+        ld0_slope_seg_1 = (laser_0_breakpoints_temp_2 - laser_0_breakpoints_temp_1) / \
+            (laser_0_breakpoints_freq_2 - laser_0_breakpoints_freq_1)
 
-        print(laser_0_breakpoints_freq, laser_0_breakpoints_temp,
-              laser_1_breakpoints_freq, laser_1_breakpoints_temp)
+        ld0_intercept_seg_1 = (laser_0_breakpoints_temp_1 - ld0_slope_seg_1*laser_0_breakpoints_freq_1)
 
-        laser_0_set_1 = [float(laser_0_breakpoints_temp[0]),
-                         float(laser_0_breakpoints_temp[1])]
-        laser_0_set_2 = [float(laser_0_breakpoints_freq[0]),
-                         float(laser_0_breakpoints_freq[1])]
+        laser_0_temp_seg_1 = ld0_slope_seg_1 * target_ghz + ld0_intercept_seg_1
 
-        laser_0_set_3 = [float(laser_0_breakpoints_temp[1]),
-                         float(laser_0_breakpoints_temp[2])]
-        laser_0_set_4 = [float(laser_0_breakpoints_freq[1]),
-                         float(laser_0_breakpoints_freq[2])]
+        #Seg 2
 
-        print("LD0")
+        ld0_slope_seg_2 = (laser_0_breakpoints_temp_3 - laser_0_breakpoints_temp_2) / \
+            (laser_0_breakpoints_freq_3 - laser_0_breakpoints_freq_2)
 
-        print("First Seg:")
+        ld0_intercept_seg_2 = (laser_0_breakpoints_temp_2 - ld0_slope_seg_2*laser_0_breakpoints_freq_2)
 
-        slope_1 = (laser_0_set_1[0] - laser_0_set_1[1]) / \
-            (laser_0_set_2[0] - laser_0_set_2[1])
+        laser_0_temp_seg_2 = ld0_slope_seg_2 * target_ghz + ld0_intercept_seg_2
 
-        intercept_1 = (laser_0_set_1[0] - slope_1*laser_0_set_2[0])
+        #LD1
+        #Seg 1
 
-        laser_0_temp_seg_1 = slope_1*target_ghz+intercept_1
+        ld1_slope_seg_1 = (laser_1_breakpoints_temp_2 - laser_1_breakpoints_temp_1) / \
+            (laser_1_breakpoints_freq_2 - laser_1_breakpoints_freq_1)
 
-        print(laser_0_temp_seg_1)
+        ld1_intercept_seg_1 = (laser_1_breakpoints_temp_1 - ld1_slope_seg_1*laser_1_breakpoints_freq_1)
 
-        print("Second Seg:")
+        laser_1_temp_seg_1 = ld1_slope_seg_1 * target_ghz + ld1_intercept_seg_1
 
-        slope_2 = (laser_0_set_3[0] - laser_0_set_3[1]) / \
-            (laser_0_set_4[0] - laser_0_set_4[1])
+        #Seg 2
 
-        intercept_2 = (laser_0_set_3[0] - slope_1*laser_0_set_4[0])
+        ld1_slope_seg_2 = (laser_1_breakpoints_temp_3 - laser_1_breakpoints_temp_2) / \
+            (laser_1_breakpoints_freq_3 - laser_1_breakpoints_freq_2)
 
-        laser_0_temp_seg_2 = slope_2*target_ghz+intercept_2
+        ld1_intercept_seg_2 = (laser_1_breakpoints_temp_1 - ld1_slope_seg_2*laser_1_breakpoints_freq_1)
 
-        print(laser_0_temp_seg_2)
+        laser_1_temp_seg_2 = ld1_slope_seg_2 * target_ghz + ld1_intercept_seg_2
 
-        print("LD1")
-
-        laser_1_set_1 = [float(laser_1_breakpoints_temp[0]),
-                         float(laser_1_breakpoints_temp[1])]
-        laser_1_set_2 = [float(laser_1_breakpoints_freq[0]),
-                         float(laser_1_breakpoints_freq[1])]
-
-        laser_1_set_3 = [float(laser_1_breakpoints_temp[1]),
-                         float(laser_1_breakpoints_temp[2])]
-        laser_1_set_4 = [float(laser_1_breakpoints_freq[1]),
-                         float(laser_1_breakpoints_freq[2])]
-
-        print("First Seg:")
-
-        slope_3 = (laser_1_set_1[0] - laser_1_set_1[1]) / \
-            (laser_1_set_2[0] - laser_1_set_2[1])
-
-        intercept_3 = (laser_1_set_1[0] - slope_3*laser_1_set_2[0])
-
-        laser_1_temp_seg_1 = slope_3*target_ghz+intercept_3
-
-        print(laser_1_temp_seg_1)
-
-        print("Second Seg:")
-
-        slope_4 = (laser_1_set_3[0] - laser_1_set_3[1]) / \
-            (laser_1_set_4[0] - laser_1_set_4[1])
-
-        intercept_4 = (laser_1_set_3[0] - slope_4*laser_1_set_4[0])
-
-        laser_1_temp_seg_2 = slope_4*target_ghz+intercept_4
-
-        print(laser_1_temp_seg_2)
-
-        if laser_0_set_1[1] >= target_ghz:
+        if laser_0_breakpoints_freq_2 >= target_ghz:
             ld0_temp = laser_0_temp_seg_1
             ld1_temp = laser_1_temp_seg_1
         else:
@@ -177,31 +144,67 @@ class SerialDataManipulation():
 
         return ld0_temp, ld1_temp
 
-    def calculate_freq_using_poly(self):
+    def calculate_freq_using_poly(self, ld0_temp, ld1_temp):
+        ld0_correction_found = False
+        ld1_correction_found = False
+        result = 0
 
-        temp = 25
-
-        coefficients = [float(self.cal_data.LD0.dwnscan_coef_seg_1[0]),
+        #LD0
+        ld0_coefficients = [float(self.cal_data.LD0.dwnscan_coef_seg_1[0]),
                         float(self.cal_data.LD0.dwnscan_coef_seg_1[1]),
                         float(self.cal_data.LD0.dwnscan_coef_seg_1[2]),
                         float(self.cal_data.LD0.dwnscan_coef_seg_1[3]),
                         float(self.cal_data.LD0.dwnscan_coef_seg_1[4]),
                         float(self.cal_data.LD0.dwnscan_coef_seg_1[5])]
 
-        actual_freq = (coefficients[0]*(temp**0) +
-                       coefficients[1]*(temp**1) +
-                       coefficients[2]*(temp**2) +
-                       coefficients[3]*(temp**3) +
-                       coefficients[4]*(temp**4) +
-                       coefficients[5]*(temp**5)
+        ld0_actual_freq = (ld0_coefficients[0]*(ld0_temp**0) +
+                       ld0_coefficients[1]*(ld0_temp**1) +
+                       ld0_coefficients[2]*(ld0_temp**2) +
+                       ld0_coefficients[3]*(ld0_temp**3) +
+                       ld0_coefficients[4]*(ld0_temp**4) +
+                       ld0_coefficients[5]*(ld0_temp**5)
+                       )
+        if ld0_actual_freq != 0:
+            ld0_correction_found = True
+
+        #LD1
+        ld1_coefficients = [float(self.cal_data.LD1.dwnscan_coef_seg_1[0]),
+                        float(self.cal_data.LD1.dwnscan_coef_seg_1[1]),
+                        float(self.cal_data.LD1.dwnscan_coef_seg_1[2]),
+                        float(self.cal_data.LD1.dwnscan_coef_seg_1[3]),
+                        float(self.cal_data.LD1.dwnscan_coef_seg_1[4]),
+                        float(self.cal_data.LD1.dwnscan_coef_seg_1[5])]
+
+        ld1_actual_freq = (ld1_coefficients[0]*(ld1_temp**0) +
+                       ld1_coefficients[1]*(ld1_temp**1) +
+                       ld1_coefficients[2]*(ld1_temp**2) +
+                       ld1_coefficients[3]*(ld1_temp**3) +
+                       ld1_coefficients[4]*(ld1_temp**4) +
+                       ld1_coefficients[5]*(ld1_temp**5)
                        )
 
-        print(actual_freq)
+        if ld1_actual_freq != 0:
+            ld1_correction_found = True
+
+        if ld0_correction_found == False or ld1_correction_found == False:
+            return 0
+        if self.cal_data.zero_cross:
+            result = abs(ld0_actual_freq - ld1_actual_freq)
+        elif self.cal_data.L1_minus_L0:
+            result = ld1_actual_freq - ld0_actual_freq
+        else: 
+            result = ld0_actual_freq - ld1_actual_freq
+
+        return result
 
     def dwell_control(self, target_ghz, time_constant):
+        start_time = time.time()
+        time_table = []
         dwell_normalized = []
         temps_read_ld0 = []
         temps_read_ld1 = []
+        lockin_1st_list = []
+        actual_ghz = []
 
         ld0_temp, ld1_temp = self.calculate_temps_for_target_ghz(target_ghz)
         print("Setting Laser Temperatures: ")
@@ -223,26 +226,34 @@ class SerialDataManipulation():
         print("Setting time constant: ")
         self.serial_commands_class.set_lockin_time_constant(time_constant)
         print("Setting gain: ")
-        self.serial_commands_class.set_lockin_gain()
+        self.serial_commands_class.set_lockin_gain(self.cal_data.gain)
         print("Enabling lockin amplifier: ")
         self.serial_commands_class.lockin_enable()
         time.sleep(0.2)
         print("Enabling laser bias: ")
         self.serial_commands_class.laser_bias_enable()
 
-        for i in range(150):
+        for i in range(200):
+            elapsed_time = time.time() - start_time
             lockin_1st, temp_read_ld0, temp_read_ld1 = self.serial_commands_class.read_lockin_1st_and_both_temps()
-            print("Lockin read: ", lockin_1st)
-            print("LD0 temp: ", temp_read_ld0)
-            print("LD1 temp: ", temp_read_ld1)
+            print("Time Stamp: ", elapsed_time)
             count, lockin_2nd = self.serial_commands_class.read_sample_count_second_lockin_output()
+            print("Lock in sample count: ", count)
             normalize_1, normalize_2 = self.normalize_lockin(
                 count, lockin_1st, lockin_2nd)
             dwell_normalized.append(normalize_1)
             temps_read_ld0.append(temp_read_ld0)
             temps_read_ld1.append(temp_read_ld1)
+            time_table.append(elapsed_time)
+            lockin_1st_list.append(lockin_1st)
+            true_ghz = self.calculate_freq_using_poly(temp_read_ld0,temp_read_ld1)
+            actual_ghz.append(true_ghz)
 
-            time.sleep(0.05)
+            #Active control
+            self.correct_for_target(true_ghz,target_ghz)
+
+
+            time.sleep(time_constant/1000 + 0.02)
 
         # time.sleep(10)
         print("Shutdown sequence: ")
@@ -255,21 +266,46 @@ class SerialDataManipulation():
         self.serial_commands_class.laser_bias_disable()
         self.serial_commands_class.lockin_disable()
 
-        print(dwell_normalized)
+        print("Time: ",time_table)
+        print("Dwell normalized: ",dwell_normalized)
+        print("Lockin raw: ",lockin_1st_list)
+        print("Actual ghz: ",actual_ghz)
+        print("LD0 temps: ", temps_read_ld0)
+        print("LD1 temps: ", temps_read_ld1)
+
+        simple_graph(time_table,dwell_normalized)
 
         self.close_port()
 
-    def correct_for_target(self, actual_ghz, target_ghz, cumulative_corrections_ghz):
-        error_freq_ghz = target_ghz - actual_ghz
-        #error_i_freq_ghz += error_freq_ghz
-        error_freq_i = 0
-
+    def correct_for_target(self, actual_ghz, target_ghz):
+        MAX_FREQ_DEVIATION_GHZ = 25
         correction_factor_p = 0.1
         correction_factor_i = 0.000001
 
-        cumulative_corrections_ghz = error_freq_ghz + \
+        error_freq_ghz = target_ghz - actual_ghz
+        #error_i_freq_ghz += error_freq_ghz
+        self.error_freq_i = self.error_freq_i + error_freq_ghz
+
+        self.set_freq = self.set_freq + \
             (correction_factor_p * error_freq_ghz) + \
-            (correction_factor_i * error_freq_i)
+            (correction_factor_i * self.error_freq_i)
+        
+        if self.set_freq > target_ghz + MAX_FREQ_DEVIATION_GHZ:
+            self.set_freq = target_ghz + MAX_FREQ_DEVIATION_GHZ
+        elif self.set_freq < target_ghz - MAX_FREQ_DEVIATION_GHZ:
+            self.set_freq = target_ghz - MAX_FREQ_DEVIATION_GHZ
+        
+        ld0_corrected , ld1_corrected = self.calculate_temps_for_target_ghz(self.set_freq)
+
+        self.serial_commands_class.set_LD0_Temperature(ld0_corrected)
+        self.serial_commands_class.set_LD1_Temperature(ld1_corrected)
+        
+        if abs(error_freq_ghz) > 0.25:
+            self.stable_count = 0
+        else:
+            self.stable_count = self.stable_count + 1
+        
+        self.error_freq_last_ghz = error_freq_ghz
 
     def dwell_timer(self):
         # self.dwell_control()
@@ -281,8 +317,6 @@ class SerialDataManipulation():
             data_sample_2nd_lockin = ((lockin_value_1/count)**2)
         else:
             data_sample_2nd_lockin = 0
-        print(data_sample_1st_lockin)
-        print(data_sample_2nd_lockin)
         return data_sample_1st_lockin, data_sample_2nd_lockin
 
     def active_control(self, temp_ld1, temp_ld2):
@@ -303,3 +337,6 @@ class SerialDataManipulation():
         print(self.cal_data.LD0.upscan_breakpoint[1])
         print(self.cal_data.LD0.cal_bias)
         print(self.cal_data.LD1.cal_bias)
+        print(self.cal_data.zero_cross)
+        print(self.cal_data.L1_minus_L0)
+        print(self.cal_data.gain)
